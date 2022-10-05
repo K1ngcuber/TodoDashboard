8a612c10aa3c61df28264f2aad31b230cdec5641
K1ngcuber <maximilian.kriegl@gmx.at>
2022-10-05 21:34:16 +0200

    [WIP] working on checkbox

--- TodoContainer.tsx
+++ TodoContainer.tsx
@@ -6,14 +6,24 @@ type Todo = {
 };
 
 export default function TodoContainer({ todos }) {
+  const handleComplete = (timestamp: string) => {
+    fetch(`http://192.168.0.24:1880/todos/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ timestamp }),
+    });
+  };
+
   return (
     <div className="todo-container">
       <div className="row">
         <div className="col">
           <h1>Todo</h1>
-          {todos.map((todo: Todo, index) => (
+          {todos.map((todo: Todo, index: any) => (
             <div key={index} className="todo">
-              <input className="form-check-input todo-checkbox" type="checkbox" value="" id={`todo_${index}`} />
+              <input onClick={() => handleComplete(todo.timestamp)} className="form-check-input todo-checkbox" type="checkbox" value="" id={`todo_${index}`} />
               <label className="form-check-label todo-text" htmlFor={`todo_${index}`}>
                 {todo.todo}
               </label>
@@ -22,7 +32,7 @@ export default function TodoContainer({ todos }) {
         </div>
         <div className="col">
           <h1>Done</h1>
-          {todos.map((todo: Todo, index) => (
+          {todos.map((todo: Todo, index: any) => (
             <div key={index} className="todo">
               <input className="form-check-input todo-checkbox" type="checkbox" value="" checked />
               <label className="form-check-label todo-text done" htmlFor="flexCheckDefault">
--- app.tsx
+++ app.tsx
@@ -1,18 +1,25 @@
 import * as React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import TodoContainer from "./TodoContainer";
 
 function App() {
   const [todos, setTodos] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("http://localhost:1880/todos")
+    fetch("http://192.168.0.24:1880/todos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((response) => response.json())
       .then((data) => setTodos(data));
   }, []);
 
+  console.log(todos);
+
   return <TodoContainer todos={todos} />;
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
