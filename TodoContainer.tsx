45dda52db3bca83072ee4c157d973bbef04b88cd
K1ngcuber <maximilian.kriegl@gmx.at>
2022-10-06 00:08:11 +0200

    [ADD] check todos

--- TodoContainer.tsx
+++ TodoContainer.tsx
@@ -3,43 +3,38 @@ import React from "react";
 type Todo = {
   todo: string;
   timestamp: string;
+  done: boolean;
 };
 
-export default function TodoContainer({ todos }) {
-  const handleComplete = (timestamp: string) => {
-    fetch(`http://192.168.0.24:1880/todos/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ timestamp }),
-    });
-  };
-
+export default function TodoContainer({ todos, handleComplete }) {
   return (
     <div className="todo-container">
       <div className="row">
         <div className="col">
           <h1>Todo</h1>
-          {todos.map((todo: Todo, index: any) => (
-            <div key={index} className="todo">
-              <input onClick={() => handleComplete(todo.timestamp)} className="form-check-input todo-checkbox" type="checkbox" value="" id={`todo_${index}`} />
-              <label className="form-check-label todo-text" htmlFor={`todo_${index}`}>
-                {todo.todo}
-              </label>
-            </div>
-          ))}
+          {todos
+            .filter((x) => !x.done)
+            .map((todo: Todo, index: any) => (
+              <div key={index} className="todo">
+                <input onClick={() => handleComplete(todo.timestamp)} className="form-check-input todo-checkbox" type="checkbox" value="" id={`todo_${index}`} />
+                <label className="form-check-label todo-text" htmlFor={`todo_${index}`}>
+                  {todo.todo}
+                </label>
+              </div>
+            ))}
         </div>
         <div className="col">
           <h1>Done</h1>
-          {todos.map((todo: Todo, index: any) => (
-            <div key={index} className="todo">
-              <input className="form-check-input todo-checkbox" type="checkbox" value="" checked />
-              <label className="form-check-label todo-text done" htmlFor="flexCheckDefault">
-                {todo.todo}
-              </label>
-            </div>
-          ))}
+          {todos
+            .filter((x) => x.done)
+            .map((todo: Todo, index: any) => (
+              <div key={index} className="todo">
+                <input className="form-check-input todo-checkbox" type="checkbox" value="" checked readOnly />
+                <label className="form-check-label todo-text done" htmlFor="flexCheckDefault">
+                  {todo.todo}
+                </label>
+              </div>
+            ))}
         </div>
       </div>
     </div>
--- app.tsx
+++ app.tsx
@@ -6,6 +6,10 @@ function App() {
   const [todos, setTodos] = React.useState([]);
 
   React.useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = () => {
     fetch("http://192.168.0.24:1880/todos", {
       method: "GET",
       headers: {
@@ -14,11 +18,19 @@ function App() {
     })
       .then((response) => response.json())
       .then((data) => setTodos(data));
-  }, []);
+  };
 
-  console.log(todos);
+  const handleComplete = (timestamp: string) => {
+    fetch(`http://192.168.0.24:1880/todos/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ timestamp }),
+    }).then(() => fetchTodos());
+  };
 
-  return <TodoContainer todos={todos} />;
+  return <TodoContainer todos={todos} handleComplete={handleComplete} />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
