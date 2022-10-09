d1f622f446811d245a96eb8185b7962c99fa1dc3
K1ngcuber <maximilian.kriegl@gmx.at>
2022-10-09 15:25:40 +0200

    [ADD] readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# TodoDashboard
+
+<img alt="TodoDashboard" src="docs/site.png" width="800" />
+
+## This is a simple dashboard for managing your todo list. 
+## It is built using [React](https://reactjs.org/) and [Node-Red](https://nodered.org/).
+
--- app.tsx
+++ app.tsx
@@ -52,7 +52,39 @@ function App() {
     });
   };
 
-  return <TodoContainer todos={todos} handleComplete={handleComplete} handleUncomplete={handleUncomplete} />;
+  return (
+    <TodoContainer
+      todos={[
+        {
+          timestamp: 1665086269089,
+          done: 1,
+          todo: "Make to do list",
+        },
+        {
+          timestamp: 1665086269089,
+          done: 1,
+          todo: "Check off first thing on list",
+        },
+        {
+          timestamp: 1665086269089,
+          done: false,
+          todo: "Realize you've already accomplished 2 things",
+        },
+        {
+          timestamp: 1665086269089,
+          done: false,
+          todo: "Reward yourself with a cookie",
+        },
+        {
+          timestamp: 1665086269089,
+          done: false,
+          todo: "Make another to do list",
+        },
+      ]}
+      handleComplete={handleComplete}
+      handleUncomplete={handleUncomplete}
+    />
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
